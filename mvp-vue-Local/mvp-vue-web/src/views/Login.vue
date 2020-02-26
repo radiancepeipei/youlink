6a5c11ca108cd0ebd0632a1550688617483c7d22
radiance <radiance6252@gmail.com>
2020-02-26 20:39:03 +0800

    vue login/logout/signup

---
+++ mvp-vue-Local/mvp-vue-web/src/views/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="login">
+    <h3>Sign in</h3>
+    <input type="text" v-model="email" placeholder="Email" /><br />
+    <input type="password" v-model="password" placeholder="Password" /><br />
+    <button @click="login">Login</button>
+    <p>
+      Don't have an account? <router-link to="/sign-up">Sign up</router-link>
+    </p>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+  name: 'login',
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          () => {
+            //alert('You are signed in!')
+            this.$router.replace('home')
+          },
+          err => {
+            alert('Oops.' + err.message)
+          }
+        )
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login {
+  margin-top: 40px;
+}
+input {
+  margin: 10px 0;
+  width: 20%;
+  padding: 15px;
+}
+button {
+  margin-top: 20px;
+  width: 10%;
+  cursor: pointer; /*hover上去會有一個手*/
+}
+p {
+  margin-top: 40px;
+  font-size: 13px;
+}
+p a {
+  text-decoration: underline;
+  cursor: pointer;
+}
+</style>

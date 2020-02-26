47bc7f24f1b1df3e582a7e64beb1db623aa0c385
radiance <radiance6252@gmail.com>
2020-02-26 20:45:58 +0800

    vue login/logout/signup

---
+++ mvp-vue-web/src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <button @click="logout">Logout</button>
+  </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+  name: 'home',
+  methods: {
+    logout: function() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.replace('login')
+        })
+    }
+  }
+}
+</script>
